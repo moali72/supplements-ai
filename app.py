65f6831a7acd130bac0df278aa7ab675ed3d01cb
moali72 <mostfaali9169011@gmail.com>
2024-10-31 23:10:48 +0200

    akjsgjhfas

--- app.py
+++ app.py
@@ -100,6 +100,7 @@ def parse_nf():
                         "type": "text",
                         "text": 
                         "extract the name, amount_per_serving and daily_value for every ingredient in table from image related to nutrition facts of certain product"
+                        "extract the name, amount_per_serving and daily_value for every ingredient in table from image related to nutrition facts of certain product"
                         "extract serving size"
                         "extract serving per container"
                         "extract ingredients under other ingredients section"
